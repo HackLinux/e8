4084cb9670eb2dbe1d8c61f6d6d88932a3ee1ce3
He Liu <liulonnie@gmail.com>
2014-03-06 13:39:58 -0800

    parser

--- stay/parser/parser.go
+++ stay/parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"io"
+	"strings"
 
 	"github.com/h8liu/e8/stay/ast"
 )
@@ -18,4 +19,9 @@ func New() *Parser {
 
 func (self *Parser) Parse(id uint8, in io.Reader) (*ast.Ast, error) {
 	panic("todo")
+}
+
+func ParseStr(s string) (*ast.Ast, error) {
+	p := New()
+	return p.Parse(0, strings.NewReader(s))
 }
