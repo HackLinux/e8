c37ca4bb6df6193f777de1b906d0385941c9ac09
He Liu <liulonnie@gmail.com>
2014-03-07 09:22:47 -0800

    docs

--- vm/inst/core.go
+++ vm/inst/core.go
@@ -1,9 +1,14 @@
 package inst
 
+// Defines the operations that a CPU box for instruction needs to implement
 type Core interface {
-	// register operations
+	// integer register operations
 	WriteReg(a uint8, v uint32)
 	ReadReg(a uint8) uint32
+
+	// floating point register operations
+	WriteFloatReg(a uint8, v float64)
+	ReadFloatReg(a uint8) float64
 
 	// memory operations
 	WriteU8(addr uint32, v uint8)
@@ -16,7 +21,7 @@ type Core interface {
 }
 
 const (
-	NintReg  = 32
-	NfloatReg = NintReg
-	RegPC = NintReg - 1
+	NintReg   = 32          // nunber of integer registers
+	NfloatReg = NintReg     // number of floating point registers
+	RegPC     = NintReg - 1 // the index of program counter
 )
