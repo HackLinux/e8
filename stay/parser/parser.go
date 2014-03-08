474f6e4523880ba6109b744c74e6331d44ddc41c
He Liu <liulonnie@gmail.com>
2014-03-08 11:00:29 -0800

    parser

--- stay/parser/parser.go
+++ stay/parser/parser.go
@@ -2,24 +2,66 @@ package parser
 
 import (
 	"io"
+	"os"
 	"strings"
 
 	"github.com/h8liu/e8/stay/ast"
+	"github.com/h8liu/e8/stay/lexer"
+	"github.com/h8liu/e8/stay/tokens"
 )
 
 type Parser struct {
+	lexErrors []*lexer.Error
 }
 
 func New() *Parser {
 	ret := new(Parser)
-	panic("todo")
 
 	return ret
 }
 
 func (self *Parser) Parse(id uint8, in io.Reader) (*ast.Ast, error) {
+	lex := lexer.New(in)
 
-	panic("todo")
+	for {
+		to, _, _ := lex.Scan()
+		if to == tokens.EOF {
+			break
+		}
+	}
+
+	e := lex.Err()
+	if e != nil {
+		return nil, e
+	}
+
+	self.lexErrors = lex.LexErrors()
+
+	if len(self.lexErrors) > 0 {
+		return nil, self.lexErrors[0]
+	}
+
+	return nil, nil
+}
+
+func ParseFile(path string) (*ast.Ast, error) {
+	fin, e := os.Open(path)
+	if e != nil {
+		return nil, e
+	}
+
+	parser := New()
+	ret, e := parser.Parse(0, fin)
+	if e != nil {
+		return nil, e
+	}
+
+	e = fin.Close()
+	if e != nil {
+		return nil, e
+	}
+
+	return ret, nil
 }
 
 func ParseStr(s string) (*ast.Ast, error) {
