3d532a4a13a4d22b9355183182ee6fc33722adec
He Liu <liulonnie@gmail.com>
2014-03-16 16:49:27 -0700

    imports

--- stay/module/module.go
+++ stay/module/module.go
@@ -1,5 +1,13 @@
 package module
 
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+)
+
 const (
 	MaxFile = 200
 )
@@ -36,9 +44,12 @@ func (maker *Maker) Open(p string) (*Module, error) {
 		if e != nil {
 			return nil, e
 		}
-		ret.imports, e = ScanImports(ret.meta.srcpath, ret.files)
+		e = ret.ScanImports()
+		if e != nil {
+			e = ret.SaveImports()
+		}
 	} else {
-		ret.imports, e = LoadImports(ret.meta.libpath)
+		e = ret.LoadImports()
 	}
 
 	if e != nil {
@@ -47,4 +58,53 @@ func (maker *Maker) Open(p string) (*Module, error) {
 
 	maker.addModule(ret)
 	return ret, nil
+}
+
+func (self *Module) ScanImports() error {
+	srcpath := self.meta.srcpath
+	files := self.files
+
+	for _, file := range files {
+		path := filepath.Join(srcpath, file)
+		println(path)
+		// TODO:
+	}
+
+	self.imports = make([]string, 0)
+	return nil
+}
+
+func (self *Module) SaveImports() error {
+	libpath := self.meta.libpath
+	path := filepath.Join(libpath, "imports")
+
+	buf := new(bytes.Buffer)
+	for _, im := range self.imports {
+		fmt.Fprintln(buf, im)
+	}
+
+	return ioutil.WriteFile(path, buf.Bytes(), 0644)
+}
+
+func (self *Module) LoadImports() error {
+	libpath := self.meta.libpath
+	path := filepath.Join(libpath, "imports")
+	bytes, e := ioutil.ReadFile(path)
+	if e != nil {
+		return e
+	}
+
+	s := string(bytes)
+	lines := strings.Split(s, "\n")
+	ret := make([]string, 0, len(lines))
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		ret = append(ret, line)
+	}
+
+	self.imports = ret
+	return nil
 }
