b9b0ced0ce55fba816ca29e9fb39d5868eb630e8
He Liu <liulonnie@gmail.com>
2014-05-29 11:14:37 -0700

    fmt

--- ast/ast.go
+++ ast/ast.go
@@ -1,25 +1,40 @@
 package ast
 
 type (
-	Decl interface {}
-	Stmt interface {}
-	Expr interface {}
-	Type interface {}
+	Decl interface{}
+	Stmt interface{}
+	Expr interface{}
+	Type interface{}
 
-	Program struct { Imports []*Import; Decls []Decl }
+	Program struct {
+		Imports []*Import
+		Decls   []Decl
+	}
 
-	Import struct { Name, Path string }
+	Import struct{ Name, Path string }
 
-	ConstDecl struct { Name string; Type Type; Expr Expr }
-	VarDecl struct { Name string; Type Type; Expr Expr }
+	ConstDecl struct {
+		Name string
+		Type Type
+		Expr Expr
+	}
+	VarDecl struct {
+		Name string
+		Type Type
+		Expr Expr
+	}
 
-	StructDecl struct { Name string; Fields []*Field }
+	StructDecl struct {
+		Name   string
+		Fields []*Field
+	}
 
-	Field struct { Name string; Type Type; Export bool }
-	BasicType struct { T int }
-	PtrType struct { Type Type }
-	StructType struct { Name string }
+	Field struct {
+		Name   string
+		Type   Type
+		Export bool
+	}
+	BasicType  struct{ T int }
+	PtrType    struct{ Type Type }
+	StructType struct{ Name string }
 )
-
-
-
