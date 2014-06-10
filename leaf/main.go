8579f22d9a628350838d064c58f08ee4eed367af
He Liu <liulonnie@gmail.com>
2014-06-10 16:04:39 -0700

    token

--- leaf/main.go
+++ leaf/main.go
@@ -1,10 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	/*
-		some commands here
-		- fmt (format .lf file)
-		- build (build from .lf file to .e8 file)
-		- run (build in temp and use e8 to run)
-	*/
+	args := os.Args
+
+	if len(args) <= 1 {
+		intro()
+		return
+	}
+
+	cmd := args[1]
+	args = args[2:]
+
+	switch cmd {
+	case "token":
+		mainToken(args)
+	case "run":
+		mainRun(args)
+	case "build":
+		mainBuild(args)
+	case "help":
+		mainHelp(args)
+	default:
+		fmt.Fprintf(os.Stderr, "leaf: unknown subcommand %q", cmd)
+		fmt.Fprintf(os.Stderr, "Run 'leaf help' for usage")
+	}
+}
+
+func mainRun(args []string) {
+	panic("todo")
+}
+
+func mainBuild(args []string) {
+	panic("todo")
+}
+
+func mainHelp(args []string) {
+	panic("todo")
+}
+
+func intro() {
+	panic("todo")
 }
---
+++ leaf/token.go
@@ -0,0 +1,5 @@
+package main
+
+func mainToken(args []string) {
+	panic("todo")
+}
