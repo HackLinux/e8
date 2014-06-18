c2933e5e9b36ed7ab7c92912c931e0c8745d4a2a
He Liu <liulonnie@gmail.com>
2014-06-17 20:37:57 -0700

    check

--- tests/check.mk
+++ tests/check.mk
@@ -1,5 +1,15 @@
 .PHONY: check build
 
-test:
-	leaf parse main.lf
+check:
+	leaf lex main.lf | diff - lex.result
+	leaf parse main.lf | diff - parse.result
 
+build:
+	leaf lex main.lf > lex.result
+	leaf parse main.lf > parse.result
+
+lex:
+	leaf lex main.lf
+
+parse:
+	leaf parse main.lf
